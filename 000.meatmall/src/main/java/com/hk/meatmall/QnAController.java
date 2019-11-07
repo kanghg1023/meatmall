bfc731ee92537e403ee38fc56778242fab85da6f
10H <10H@DESKTOP-BFA9EH3>
2019-11-07 22:53:10 +0900

    cc

--- 000.meatmall/src/main/java/com/hk/meatmall/QnAController.java
+++ 000.meatmall/src/main/java/com/hk/meatmall/QnAController.java
@@ -7,7 +7,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -275,39 +274,41 @@ private static final Logger logger = LoggerFactory.getLogger(QnAController.class
 			//한글깨짐을 방지하기위해 문자셋 설정
 			response.setCharacterEncoding("utf-8");
 		 
-		        // 마찬가지로 파라미터로 전달되는 response 객체의 한글 설정
-		        response.setContentType("text/html; charset=utf-8");
-		 
-		        // 업로드한 파일 이름
-		        String fileName = upload.getOriginalFilename();
-		       
-		        String stored_fname = Util.createUUId()
-				         +(fileName.substring(fileName.lastIndexOf(".")));
-		        // 파일을 바이트 배열로 변환
-		        byte[] bytes = upload.getBytes();
-		 
-		        // 이미지를 업로드할 디렉토리(배포 디렉토리로 설정)
+	        // 마찬가지로 파라미터로 전달되는 response 객체의 한글 설정
+	        response.setContentType("text/html; charset=utf-8");
+	 
+	        // 업로드한 파일 이름
+	        String fileName = upload.getOriginalFilename();
+	       
+	        String stored_fname = Util.createUUId()
+			         +(fileName.substring(fileName.lastIndexOf(".")));
+	        // 파일을 바이트 배열로 변환
+	        byte[] bytes = upload.getBytes();
+	 
+	        // 이미지를 업로드할 디렉토리(배포 디렉토리로 설정)
 //		        String uploadPath ="D:\\java_lec_2class\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp1\\wtpwebapps\\000.meatmall4ck\\images\\";
 //		        String uploadPath ="D:\\eclips\\딱따구리\\000.meatmalltest\\src\\main\\webapp\\resources\\ckimages\\";
-		        String uploadPath ="C:\\Users\\HKEDU\\git\\meatmall\\000.meatmall\\src\\main\\webapp\\resources\\ckimages\\";
-		        OutputStream out = new FileOutputStream(new File(uploadPath + stored_fname));
-		        
-		        // 서버로 업로드
-		        // write메소드의 매개값으로 파일의 총 바이트를 매개값으로 준다.
-		        // 지정된 바이트를 출력 스트립에 쓴다 (출력하기 위해서)
-		        out.write(bytes);
-		        	        
-		        // 서버=>클라이언트로 텍스트 전송(자바스크립트 실행)
-		        PrintWriter printWriter = response.getWriter();
-		        
-		        String fileUrl = "/meatmall/ckimages/" + stored_fname;	     
-		        System.out.println(fileUrl);
-		        printWriter.println("{\"fileName\" : \""+stored_fname+"\", \"uploaded\" : 1, \"url\":\""+fileUrl+"\"}");     
-		        printWriter.flush();
-		        out.close();
-		        printWriter.close(); 
-		        return null;
-		    }	
+	        String uploadPath ="C:\\Users\\HKEDU\\git\\meatmall\\000.meatmall\\src\\main\\webapp\\resources\\ckimages\\";
+//한결 집	    String uploadPath ="C:\\Users\\10H\\git\\meatmall\\000.meatmall\\src\\main\\webapp\\resources\\ckimages\\";
+	        
+	        OutputStream out = new FileOutputStream(new File(uploadPath + stored_fname));
+	        
+	        // 서버로 업로드
+	        // write메소드의 매개값으로 파일의 총 바이트를 매개값으로 준다.
+	        // 지정된 바이트를 출력 스트립에 쓴다 (출력하기 위해서)
+	        out.write(bytes);
+	        	        
+	        // 서버=>클라이언트로 텍스트 전송(자바스크립트 실행)
+	        PrintWriter printWriter = response.getWriter();
+	        
+	        String fileUrl = "/meatmall/ckimages/" + stored_fname;	     
+	        System.out.println(fileUrl);
+	        printWriter.println("{\"fileName\" : \""+stored_fname+"\", \"uploaded\" : 1, \"url\":\""+fileUrl+"\"}");     
+	        printWriter.flush();
+	        out.close();
+	        printWriter.close(); 
+	        return null;
+	    }	
 		 
 			
 		
